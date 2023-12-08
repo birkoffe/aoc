803940275a73bcd97e30be8a80a3bae8b6e52a1f
birkoff.anarchist <birkoff.anarchist@gmail.com>
2023-12-08 12:21:48 +0700

    2023d1p2

--- 2023/day_1/main.go
+++ 2023/day_1/main.go
@@ -43,8 +43,63 @@ func solve_part1(scanner *bufio.Scanner) int {
 	return sum
 }
 
+func solve_part2(scanner *bufio.Scanner) int {
+	digits := map[string]string{
+		"1":     "1",
+		"2":     "2",
+		"3":     "3",
+		"4":     "4",
+		"5":     "5",
+		"6":     "6",
+		"7":     "7",
+		"8":     "8",
+		"9":     "9",
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
+
+	var sum int
+
+	for scanner.Scan() {
+		var a, b string
+
+		for idx := range scanner.Text() {
+			for key, value := range digits {
+				if len(key) <= len(scanner.Text()[idx:]) {
+					if scanner.Text()[idx:idx+len(key)] == key {
+						if a == "" {
+							a = value
+						}
+
+						b = value
+					}
+				}
+			}
+		}
+
+		x, err := strconv.Atoi(a + b)
+		if err != nil {
+			fmt.Printf("Error parsing %s: %s\n", a+b, err)
+			os.Exit(1)
+		}
+
+		sum += x
+	}
+
+	return sum
+}
+
 func main() {
-	files := []string{"sample_part_1.txt", "input_part_1.txt"}
+	fmt.Println("2023d1")
+
+	files := []string{"sample_part_1.txt", "input.txt"}
 	for _, file := range files {
 		f, err := os.Open(file)
 		if err != nil {
@@ -56,6 +111,21 @@ func main() {
 		scanner := bufio.NewScanner(f)
 		result := solve_part1(scanner)
 
-		fmt.Println("=", result)
+		fmt.Printf("%20s = %d\n", file, result)
+	}
+
+	files = []string{"sample_part_2.txt", "input.txt"}
+	for _, file := range files {
+		f, err := os.Open(file)
+		if err != nil {
+			fmt.Printf("Fail to open input.txt: %s\n", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+
+		scanner := bufio.NewScanner(f)
+		result := solve_part2(scanner)
+
+		fmt.Printf("%20s = %d\n", file, result)
 	}
 }
